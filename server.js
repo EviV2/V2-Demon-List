47e7d63fcdf0ce45bf186e75271804389d93fc9e
elias <144105283+EviV2@users.noreply.github.com>
2025-09-09 19:20:15 +0200

    s

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const ADMIN_USER = process.env.ADMIN_USER;
 const ADMIN_PASS = process.env.ADMIN_PASS;
 
 console.log("Admin:", ADMIN_USER); // juste pour tester
+console.log("Pass:", ADMIN_PASS ? "******" : "NOT SET"); // juste pour tester
